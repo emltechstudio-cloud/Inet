560384efce6fb3d662c8edef69e5cd2aa1d70e70
emltechstudio-cloud <emltechstudio@gmail.com>
2026-09-18 11:06:18 +0100

    Delete routers directory

--- routers/flink/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Flink backend router package."""
-from fastapi import APIRouter
-
-from .auth import router as auth_router
-from .signaling import router as signaling_router
-
-router = APIRouter()
-router.include_router(auth_router, prefix="/auth")
-router.include_router(signaling_router, prefix="/signaling")
